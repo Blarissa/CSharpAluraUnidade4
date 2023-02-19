21be3e8ed769a9a7f10614f869d0ccc354e02606
Blarissa <larissabrasil009@gmail.com>
2023-02-19 04:16:39 -0300

    :tada: init: Iniciando solução para o curso de ASP .NET Core

---
+++ Web/Web/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var livros = new List<Livro>();
+
+            livros.Add(new Livro("001", "Quem Mexeu na Minha Query?", 12.99m));
+            livros.Add(new Livro("002", "Fique Rico com C#", 30.99m));
+            livros.Add(new Livro("003", "Java Para Baixinhos", 25.99m));
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Olá, Mundo!");
+            });
+        }
+    }
+}
