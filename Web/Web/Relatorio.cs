d3f1577daf99cd382d123e4de4ae9102a17ea089
Blarissa <larissabrasil009@gmail.com>
2023-02-19 04:56:02 -0300

    :sparkles: feat Criando classe Relatorio.cs

---
+++ Web/Web/Relatorio.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public class Relatorio
+    {
+        private readonly Catalogo catalogo;
+
+        public Relatorio(Catalogo catalogo)
+        {
+            this.catalogo = catalogo;
+        }
+
+        public async Task Imprimir(HttpContext context)
+        {
+            foreach (var livro in catalogo.GetLivros())
+            
+                await context.Response.WriteAsync(
+                    $"{livro.Codigo,-10} " +
+                    $"{livro.Nome,-40} " +
+                    $"{livro.Preco,10:C}\r\n");
+            
+        }
+    }
+}
