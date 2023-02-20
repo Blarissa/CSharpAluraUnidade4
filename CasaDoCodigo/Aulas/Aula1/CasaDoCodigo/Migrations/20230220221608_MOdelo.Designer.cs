d80c08cbb4309d3d88b8d1b9f8b911e842dcfe01
Blarissa <larissabrasil009@gmail.com>
2023-02-20 19:18:15 -0300

    feat: adicionando relacionamento entre os modelos

--- CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Migrations/20230220221608_MOdelo.Designer.cs
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Migrations/20230220221608_MOdelo.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CasaDoCodigo.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230220221608_MOdelo")]
+    partial class MOdelo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
