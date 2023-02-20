4cbf73857e5a085c7202267393759d4f9d71173f
Blarissa <larissabrasil009@gmail.com>
2023-02-19 21:18:02 -0300

    :truck: chore: Modificando locais

--- web/Catalogo.cs
+++ web/Catalogo.cs
@@ -10,9 +10,11 @@ namespace web
         public List<Livro> GetLivros()
         {
             var livros = new List<Livro>();
+
             livros.Add(new Livro("001", "Quem Mexeu na Minha Query?", 12.99m));
             livros.Add(new Livro("002", "Fique Rico com C#", 30.99m));
             livros.Add(new Livro("003", "Java Para Baixinhos", 25.99m));
+
             return livros;
         }
     }
