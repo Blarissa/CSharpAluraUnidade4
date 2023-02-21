0ea6fa1626167b909beabf2a29fd6c66cc7da55a
Blarissa <larissabrasil009@gmail.com>
2023-02-21 02:18:14 -0300

    refactor: injeção de dependência na criação do banco de dados

---
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -0,0 +1,21 @@
+﻿namespace CasaDoCodigo
+{
+    public partial class Startup
+    {
+        class DataService : IDataService
+        {
+            private readonly ApplicationContext context;
+
+            public DataService(ApplicationContext context)
+            {
+                this.context = context;
+            }
+
+            public void InicializaDB()
+            {
+                context.Database.EnsureCreated();
+            }
+
+        }
+    }
+}
