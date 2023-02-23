c92653db2a9deaffc6769ef19cf96c9d2e7224e9
Blarissa <larissabrasil009@gmail.com>
2023-02-23 02:05:00 -0300

    feat: implementando PedidoRepository

---
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -0,0 +1,19 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class PedidoRepository : BaseRepository<Cadastro>, IPedidoRepository
+    {
+        public PedidoRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+    }
+
+    public interface IPedidoRepository
+    {
+    }
+}
